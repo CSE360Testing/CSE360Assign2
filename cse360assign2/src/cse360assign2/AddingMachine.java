42f5abf14536488cfb7fd0c9fdd734fc2808d404
Justin Colyar <jcolyar@asu.edu>
2019-10-07 10:44:32 -0700

    Finished changes to AddingMachine.java, updating clear(), toString(), subtract(), add(), getTotal(), and the constructor.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -2,17 +2,22 @@
  * Name:			Justin Colyar
  * Class ID:		CSE 360-70641
  * Assignment:		2
- * Description:		The adding machine is a base class that is incomplete,
- * 					but presents an outline of the constructor and various
- * 					public methods. 
+ * Description:		The AddingMachine.java file contains the class for
+ * 					AddingMachine and all of its function definitions that
+ * 					allow the user to track a running total that they 
+ * 					may retrieve, add to, subtract from, or clear and also
+ * 					print the transaction history.
  */
 
 package cse360assign2;
 
 /**
- * The class AddingMachine currently has little functionality but provides
- * the outline for the constructor as well as the getTotal, add, sub, toString,
- * and clear functions.
+ * The class AddingMachine keeps track of a running total alongside
+ * a history of the transaction that lead to the total. The class allows
+ * the user to add or subtract a specified amount to the total, print out
+ * the transaction history, retrieve the total, or clear the transaction
+ * history and total. The default total is 0 when instantiated and "0" for
+ * the transaction history.
  * 
  * @author Justin Colyar
  *
@@ -20,58 +25,77 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String transactionHistory;
 	
 	/**
 	 *  AddingMachine is the sole and default constructor for
-	 *  the AddingMachine class. It sets the total equal to 0.
+	 *  the AddingMachine class. It sets the total equal to 0
+	 *  whie also setting the String transactionHistory equal to
+	 *  "0"
 	 */
 	public AddingMachine () {
+		transactionHistory = "0";
 		total = 0;  // not needed - included for clarity
 	}
 	
 	/**
-	 * the getTotal() function returns 0.
+	 * the getTotal() function returns the current total
+	 * stored in the 'total' integer.
 	 * 
-	 * @return 0
+	 * @return total
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	/**
-	 * The add function takes an integer parameter but
-	 * currently is not functional.
+	 * The add function takes an integer parameter value and adds
+	 * it to the running total integer. It also updates the string
+	 * transactionHistory to include " + " followed by the amount
+	 * added to the total. 
 	 * 
 	 * @param value
 	 */
 	public void add (int value) {
+		total = total + value;
 		
+		// updates transactionHistory to include " + [value]"
+		transactionHistory = transactionHistory + " + " + value;
 	}
 	
 	/**
 	 * The subtract function takes an integer parameter
-	 * and is currently empty. 
+	 * value and adds it to the running total integer and also
+	 * updates the transactionHistory with " - " followed by the
+	 * amount subtracted.
 	 * 
 	 * @param value
 	 */
 	public void subtract (int value) {
+		total = total - value;
 		
+		// updates transactionHistory to include " - [value]"
+		transactionHistory = transactionHistory + " - " + value;
 	}
 	
 	/**
-	 * The toString() method returns the string ""
+	 * The toString() method returns the string transactionHistory, which
+	 * actively contains the total summary of transactions in the format
+	 * "0", followed by the add or subtract symbol, and the amount (i.e.
+	 * "0 + 5 - 2").
 	 * 
-	 * 	@return ""
+	 * 	@return transactionHistory
 	 */
 	public String toString () {
-		return "";
+		return transactionHistory;
 	}
 	
 	/**
-	 * The clear() function is unimplemented and 
-	 * has no functionality. 
+	 * The clear() function resets the running total to 0
+	 * and also resets the transactionHistory string to "0"
 	 */
 	public void clear() {
-	
+		total = 0;
+		transactionHistory = "0";
 	}
 }
